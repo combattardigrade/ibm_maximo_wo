4d45e5bbf2b24b2be13715265cc40ca820a8e136
combattardigrade <combattardigrade@gmail.com>
2020-03-02 13:18:40 -0600

    login system

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,46 +2,59 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { login } from '../utils/api'
 import { saveToken } from '../actions/auth'
-import { Dialogs } from '@ionic-native/dialogs/';
 
 import { IonInput, IonContent, IonItem, IonLabel, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import { save } from 'ionicons/icons';
+import { Plugins } from '@capacitor/core';
+const { Modals } = Plugins;
 
 class Login extends Component {
 
     
 
     componentDidMount() {
-        console.log()
+        console.log(this.props.history)
         
     }
-    
+    async showAlert(message) {
+        let alertRet = await Modals.alert({
+          title: 'Error',
+          message,
+        })
+      }
 
     handleSubmit = async (e) => {
         e.preventDefault()
         
         const user = e.target.user.value
         const password = e.target.password.value
-        Dialogs.alert('test').then(() => console.log('clicked')).catch(e => console.log(e))
-        if(!user || !password) {
-            // show error message
-           
-            
+        
+        if(!user || !password) {            
+            this.showAlert('Ingresa todos los campos requeridos')           
             return
         }
         
-        let response = await (await login({user: user, password: password})).json()
-        console.log(response)
+        let response 
+        try {
+            response = await (await login({user: user, password: password})).json()
+            console.log(response)
+        }
+        catch(err) {
+            console.log(err)
+            this.showAlert('Ocurrió un error al intentar realizar la acción. Por favor, inténtalo nuevamente.')   
+            return
+        }     
+
         if(response.status != 'OK') {
-            // show error message
-            
+            this.showAlert('message' in response ? response.message : 'Ocurrió un error al intentar realizar la acción. Por favor, inténtalo nuevamente.')   
             return
         }        
         // save jwt
         this.props.dispatch(saveToken(response.payload))
 
         // redirect to dashboard
+        this.props.history.replace('/wo')
     }
 
     
