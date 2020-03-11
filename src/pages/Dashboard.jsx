8ddb6b10c7733300765d4a9e03723ae70bfd72e4
combattardigrade <combattardigrade@gmail.com>
2020-03-10 21:51:05 -0600

    refresh btn in dashboard and improve fetching data process

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,9 +1,9 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { IonSpinner, IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar, IonItem, IonLabel, IonRefresher, IonRefresherContent, IonGrid, IonRow, IonCol } from '@ionic/react';
-
-import { RouteComponentProps } from 'react-router';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonSpinner, IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar, IonIcon, IonButton, IonItem, IonLabel, IonRefresher, IonRefresherContent, IonGrid, IonRow, IonCol } from '@ionic/react';
+import {
+    refreshOutline
+} from 'ionicons/icons'
 import './Page.css';
 import './Maximo.css'
 import { getWorkOrders, getWhoAmI, getInventory, getAssets, getWorkOrder, getWOSafety } from '../utils/api'
@@ -17,9 +17,85 @@ const { Modals } = Plugins
 
 class Dashboard extends Component {
 
-    componentDidMount() {
-        const { token, dispatch } = this.props
+    state = {
+        loading: true
+    }
 
+    componentDidMount() {
+        const { token, dispatch, workOrders, user, inventory, assets } = this.props
+
+        if(workOrders && user && inventory && assets) {
+            this.setState({loading: false})
+            return
+        }
+
+        if (!workOrders) {
+            getWorkOrders({ token: token })
+                .then(data => data.json())
+                .then(response => {
+                    if (!('status' in response) || response.status == 'ERROR') {
+                        console.log(response)
+                        this.showAlert('Ocurrió un error al intentar obtener las órdenes de trabajo. Por favor, inténtalo nuevamente')
+                        return
+                    }
+                    dispatch(saveWorkOrders(response.payload))
+                    this.setState({ loading: false })
+                })
+                .catch((err) => {
+                    console.log(err)
+                    this.showAlert('Ocurrió un error al intentar obtener las órdenes de trabajo. Por favor, inténtalo nuevamente')
+                    return
+                })
+        }
+
+
+        if (!user) {
+            getWhoAmI({ token: token })
+                .then(data => data.json())
+                .then(response => {
+                    if (!('status' in response) || response.status == 'ERROR') {
+                        console.log(response)
+                        return
+                    }
+                    dispatch(saveUser(response.payload))
+                })
+                .catch((err) => {
+                    console.log(err)
+                })
+        }
+
+        if(!inventory) {
+            getInventory({ token: token })
+            .then(data => data.json())
+            .then((response) => {
+                if (response.status == 'OK') {
+                    dispatch(saveInventory(response.payload))
+                }
+            })
+            .catch((err) => console.log(err))
+        }
+
+        if(!assets) {
+            getAssets({ token: token })
+            .then(data => data.json())
+            .then((response) => {
+                if (response.status == 'OK') {
+                    dispatch(saveAssets(response.payload))
+                }
+            })
+            .catch((err) => console.log(err))
+        }
+
+    }
+
+    handleWorkOrderClick = async (wonum) => {
+        this.props.history.push('/wo/' + wonum)
+    }
+
+    handleRefreshClick = async (e) => {
+        e.preventDefault()
+        const { token, dispatch } = this.props
+        this.setState({ loading: true })
         getWorkOrders({ token: token })
             .then(data => data.json())
             .then(response => {
@@ -29,69 +105,13 @@ class Dashboard extends Component {
                     return
                 }
                 dispatch(saveWorkOrders(response.payload))
+                this.setState({ loading: false })
             })
             .catch((err) => {
                 console.log(err)
                 this.showAlert('Ocurrió un error al intentar obtener las órdenes de trabajo. Por favor, inténtalo nuevamente')
                 return
             })
-
-
-        getWhoAmI({ token: token })
-            .then(data => data.json())
-            .then(response => {
-                if (!('status' in response) || response.status == 'ERROR') {
-                    console.log(response)
-                    return
-                }
-                dispatch(saveUser(response.payload))
-            })
-            .catch((err) => {
-                console.log(err)
-            })
-
-        getInventory({ token: token })
-            .then(data => data.json())
-            .then((response) => {
-                if (response.status == 'OK') {
-                    dispatch(saveInventory(response.payload))
-                }
-            })
-            .catch((err) => console.log(err))
-
-        getAssets({ token: token })
-            .then(data => data.json())
-            .then((response) => {
-                if (response.status == 'OK') {
-                    dispatch(saveAssets(response.payload))
-                }
-            })
-            .catch((err) => console.log(err))
-
-    }
-
-    handleWorkOrderClick = async (wonum) => {
-        // const { token, dispatch } = this.props
-        // got to wo details
-        this.props.history.push('/wo/' + wonum)
-        // getWorkOrder({ wonum: wonum, token: token })
-        //     .then((data) => data.json())
-        //     .then((response) => {
-        //         if (response.status == 'OK') {
-        //             dispatch(saveCurrentWorkOrder(response.payload))
-        //             // this.setState({ loading: false })
-        //         }
-        //     })
-
-        // getWOSafety({ wonum: wonum, token: token })
-        //     .then((data) => data.json())
-        //     .then((response) => {
-        //         if (response.status == 'OK') {
-        //             dispatch(saveWorkOrderSafety(response.payload))
-
-        //         }
-        //     })
-
     }
 
     async showAlert(message) {
@@ -103,12 +123,7 @@ class Dashboard extends Component {
 
     render() {
         const { workOrders } = this.props
-
-        // if (!workOrders) {
-        //     return <div className="spinnerCenter">
-        //         <IonSpinner name="crescent" style={{ textAlign: 'center' }} />
-        //     </div>
-        // }
+        const { loading } = this.state
 
         return (
             <IonPage>
@@ -118,6 +133,9 @@ class Dashboard extends Component {
                             <IonMenuButton />
                         </IonButtons>
                         <IonTitle>Dashboard</IonTitle>
+                        <IonButtons slot="end">
+                            <IonButton onClick={this.handleRefreshClick}><IonIcon icon={refreshOutline}></IonIcon></IonButton>
+                        </IonButtons>
                     </IonToolbar>
                 </IonHeader>
 
@@ -132,9 +150,9 @@ class Dashboard extends Component {
                     </IonRefresher>
 
                     {
-                        workOrders ?
+                        workOrders && loading == false ?
                             workOrders.map((wo) => (
-                                <WoCard key={wo.wonum} wo={wo} handleWorkOrderClick={this.handleWorkOrderClick} />                                
+                                <WoCard key={wo.wonum} wo={wo} handleWorkOrderClick={this.handleWorkOrderClick} />
                             ))
                             :
                             <div className="spinnerCenter">
@@ -150,11 +168,13 @@ class Dashboard extends Component {
 };
 
 
-function mapStateToProps({ auth, workOrders }) {
+function mapStateToProps({ auth, workOrders, user, inventory, assets }) {
     return {
         token: auth.token,
-        workOrders: workOrders && workOrders.workOrders
-
+        workOrders: workOrders && workOrders.workOrders,
+        user,
+        inventory,
+        assets
     }
 }
 
