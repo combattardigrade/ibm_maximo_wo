f97e3fc3034bc8a06b75330c178bcfd8b612dcb8
combattardigrade <combattardigrade@gmail.com>
2020-05-09 20:09:21 -0500

    add comments

--- src/pages/CommentModal.jsx
+++ src/pages/CommentModal.jsx
@@ -7,10 +7,14 @@ import {
     IonModal, IonButton, IonInput, IonSelect, IonSelectOption, IonTextarea, IonAlert
 } from '@ionic/react';
 import {
-    searchOutline
+    micCircleOutline
 } from 'ionicons/icons'
 
+// Actions
 import { saveComment } from '../actions/localWorkOrders'
+
+// Plugins
+import { SpeechRecognition } from '@ionic-native/speech-recognition'
 
 class CommentModal extends Component {
 
@@ -38,9 +42,66 @@ class CommentModal extends Component {
             comment: comments,
             wonum: currentWorkOrder.wonum
         }
-       
+
         dispatch(saveComment(comment))
         this.props.handleToggleCommentModal(false)
+    }
+
+    handleCommentsSpeech = async (e) => {
+        e.preventDefault()
+        console.log('COMMENTS_SPEECH_RECOGNITION')
+
+        // Check if another speech recognition process is active
+        // if (this.state.descriptionRecognitionActive === true || this.state.commentsRecognitionActive === true) {
+        //     this.showAlert('Otro proceso de reconocimiento de voz se encuentra activo', 'Error')
+        //     return
+        // }
+
+        // Check if feature is available
+        const available = await SpeechRecognition.isRecognitionAvailable()
+
+        if (!available) {
+            this.showAlert('Reconocimiento de voz no disponible en el dispositivo', 'Error')
+            return
+        }
+
+        // Check permission
+        let hasPermission = await SpeechRecognition.hasPermission()
+
+        if (!hasPermission) {
+            console.log('SPEECH RECOGNITION DOES NOT HAVE PERMISSION')
+            console.log('REQUESTING PERMISSION')
+            await SpeechRecognition.requestPermission()
+        }
+
+        // Check if user provided permission
+        hasPermission = await SpeechRecognition.hasPermission()
+        if (!hasPermission) {
+            this.showAlert('La aplicación no tiene permisos para realizar la acción', 'Error')
+            return
+        }
+
+        // this.setState({ descriptionRecognitionActive: true })
+
+        // Configure options
+        const options = {
+            language: 'es-MX',
+            matches: 50000
+        }
+
+        // Start recognition process
+        SpeechRecognition.startListening(options)
+            .subscribe(
+                (matches) => {
+                    console.log(matches)
+                    this.setState({ comments: this.state.comments + ' ' + matches[0] })
+                },
+                (err) => {
+                    console.log(err)
+                    this.showAlert(err, 'Error')
+                    // this.setState({ descriptionRecognitionActive: false })
+                }
+            )
     }
 
     showAlert = (msg, title) => {
@@ -57,13 +118,23 @@ class CommentModal extends Component {
                 </IonHeader>
 
                 <IonContent>
-                    <IonItem lines="none">
-                        <IonTextarea
-                            placeholder="Ingresa un comentario adicional..."
-                            onIonChange={this.handleCommentChange}
-                            value={this.state.comments}
-                        >
-                        </IonTextarea>
+                    <IonItem lines="full">
+                        <IonGrid>
+                            <IonRow>
+                                <IonCol>
+                                    <IonTextarea
+                                        placeholder="Ingresa un comentario adicional..."
+                                        onIonChange={this.handleCommentChange}
+                                        value={this.state.comments}
+                                        rows="15"
+                                    >
+                                    </IonTextarea>
+                                    <div style={{ float: 'right' }}>
+                                        <IonButton onClick={this.handleCommentsSpeech}><IonIcon icon={micCircleOutline} /></IonButton>
+                                    </div>
+                                </IonCol>
+                            </IonRow>
+                        </IonGrid>
                     </IonItem>
 
 
